d8fab747ea645cc8d3f18964def6d39db12d1b5d
unknown <eesuolaayot@gmail.com>
2023-08-01 23:12:11 +0100

    fff

--- index.js
+++ index.js
@@ -1,51 +1,37 @@
-const express = require('express')
+
+const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios')
+const axios = require('axios');
 const bodyParser = require('body-parser');
 
-require('dotenv').config()
-const PORT = 4000
+require('dotenv').config();
+const PORT = 4000;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-
 // Create a new bot instance
-const {TOKEN, SERVER_URL} = process.env
-const URI = `/webhook/${TOKEN}`
-const webhookURL = `${SERVER_URL || 'https://testing-one-coral.vercel.app'}${URI}`
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
+const { TOKEN, SERVER_URL } = process.env;
+const URI = `/webhook/${TOKEN}`;
+const webhookURL = `${SERVER_URL || 'https://testing-one-coral.vercel.app/'}${URI}`;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 
-
-// configuring the bot via Telegram API to use our route below as webhook
+// Configuring the bot via Telegram API to use our route below as a webhook
 const setupWebhook = async () => {
   try {
-      const { data } = await axios.get(`${TELEGRAM_API}/setWebhook?url=${webhookURL}&drop_pending_updates=true`)
-      console.log(data)
-      
+    const { data } = await axios.post(`${TELEGRAM_API}/setWebhook`, {
+      url: webhookURL,
+      drop_pending_updates: true,
+    });
+    console.log(data);
   } catch (error) {
-      return error
+    return error;
   }
-}
+};
 
+// Creating a bot instance with polling disabled for better webhook handling
 const bot = new TelegramBot(TOKEN, { polling: false });
-
-// app.get('/',(req, res)=> {
-//   res.send('working')
-// })
-
-app.post(URI, async(req, res) => {
-  const update = req.body
-  if (update.message && update.message.text) {
-    const text = update.message.text
-    if (text === '/start') {
-      console.log(`w`)
-    }
-  }
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
 
 const commands = [
   // [{text: 'Balance', callback_data: 'balance'},{text: 'Add Fund', callback_data: 'add_fund'}],
@@ -59,53 +45,24 @@ const keyboard = {
  
 };
 
-// we're using the API's in-built "onText" method
-// it takes in a regex and the message
-bot.onText(/\/start/, async(msg) => {
-
-  const chatId = msg.chat.id
-
-  // listens for "/start" and responds with the greeting below.
-  
-  // bot.sendMessage(chatId, 'Please select a command:', { reply_markup: keyboard });
-
-  await axios.post(`${TELEGRAM_API}/sendMessage`, {
-    chat_id : chatId,
-    text: 'Please select a command:',
-    reply_markup: JSON.stringify(keyboard)
-  })
-
-});
-
-
-
-
-bot.on('callback_query', async(callbackQuery)=> {
-  const message = callbackQuery.message;
-  const category = callbackQuery.data
-
-  console.log(category)
-  console.log(message)
-
-  // if(category !== 'undefined') bot.sendMessage(message.chat.id, `You sent a request of ${category}`)
-  if(category !== 'undefined') {
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-      chat_id : message.chat.id,
-      text: `You sent a request of ${category}`,
-    })
+app.post(URI, (req, res) => {
+  const update = req.body;
+  const chatId = req.body.message.chat.id
+  console.log(chatId)
+  if (update.message && update.message.text) {
+    const text = update.message.text;
+    if (text === '/start') {
+      console.log(update);
+      bot.sendMessage(chatId, 'Please select a command:', {reply_markup: JSON.stringify(keyboard)})
+    }
   }
-})
-
-
-
-app.listen( process.env.PORT || PORT, async () => {
-  console.log(`Express server listening on port ${PORT}`);
-  await setupWebhook()
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
+// ... Rest of your code for handling commands and callback queries ...
 
-
-
-
-
-
+app.listen(process.env.PORT || PORT, async () => {
+  console.log(`Express server listening on port ${PORT}`);
+  await setupWebhook();
+});
