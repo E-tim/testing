220c7961b5c64e5580e45656b8259c2673531cf3
unknown <eesuolaayot@gmail.com>
2023-08-07 02:37:43 +0100

    jjj

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 
-const express = require('express');
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const WebSocket  = require('ws')
+// const express = require('express');
+// const TelegramBot = require('node-telegram-bot-api');
+// const axios = require('axios');
+// const bodyParser = require('body-parser');
+// const WebSocket  = require('ws')
 
-require('dotenv').config();
-const PORT = 4000;
+// require('dotenv').config();
+// const PORT = 4000;
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-// Create a new bot instance
-const { TOKEN, SERVER_URL } = process.env;
-const URI = `/webhook/${TOKEN}`;
-const webhookURL = `${SERVER_URL || 'https://testing-one-coral.vercel.app/'}${URI}`;
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-const admin = require('firebase-admin')
-const serviceAccount  = require('./serviceAccountKey.json')
+// // Create a new bot instance
+// const { TOKEN, SERVER_URL } = process.env;
+// const URI = `/webhook/${TOKEN}`;
+// const webhookURL = `${SERVER_URL || 'https://testing-one-coral.vercel.app/'}${URI}`;
+// const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+// const admin = require('firebase-admin')
+// const serviceAccount  = require('./serviceAccountKey.json')
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+// const bot = new TelegramBot(TOKEN, { polling: true });
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-const db = admin.firestore();
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+// const db = admin.firestore();
 
-const commands = [
-  // [{text: 'Balance', callback_data: 'balance'},{text: 'Add Fund', callback_data: 'add_fund'}],
-  { text: 'Check CC', callback_data: 'checker' },
-  { text: 'Buy Fullz', callback_data: 'fullz' },
-];
-const keyboard = {
-  inline_keyboard: commands.map((command) => [{ text: command.text, callback_data: command.callback_data }])
+// const commands = [
+//   // [{text: 'Balance', callback_data: 'balance'},{text: 'Add Fund', callback_data: 'add_fund'}],
+//   { text: 'Check CC', callback_data: 'checker' },
+//   { text: 'Buy Fullz', callback_data: 'fullz' },
+// ];
+// const keyboard = {
+//   inline_keyboard: commands.map((command) => [{ text: command.text, callback_data: command.callback_data }])
  
-};
+// };
 
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Welcome to Xstore !', {reply_markup: keyboard});
-});
+// bot.onText(/\/start/, (msg) => {
+//   bot.sendMessage(msg.chat.id, 'Welcome to Xstore !', {reply_markup: keyboard});
+// });
 
-bot.on('callback_query', (query)=> {
-  const chatId = query.message.chat.id;
-  const data = query.data
+// bot.on('callback_query', (query)=> {
+//   const chatId = query.message.chat.id;
+//   const data = query.data
 
-  if (data === 'checker') {
-    bot.sendMessage(chatId, `you selected ${data}`)
-  }
-})
+//   if (data === 'checker') {
+//     bot.sendMessage(chatId, `you selected ${data}`)
+//   }
+// })
 
-app.get('/', (req, res) => {
-  res.send('Telegram Bot is running!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Telegram Bot is running!');
+// });
 
 
 // const webhookURLs = `https://qwewew-6b05de536ab3.herokuapp.com/${TOKEN}`;
@@ -61,11 +61,11 @@ app.get('/', (req, res) => {
 
 
 
-// ... Rest of your code for handling commands and callback queries ...
+// // ... Rest of your code for handling commands and callback queries ...
 
-app.listen(process.env.PORT || PORT, async () => {
-  console.log(`Express server listening on port ${PORT}`);
-  // await setupWebhook();
-});
+// app.listen(process.env.PORT || PORT, async () => {
+//   console.log(`Express server listening on port ${PORT}`);
+//   // await setupWebhook();
+// });
 
 
